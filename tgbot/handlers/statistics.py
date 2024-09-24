3e8157b4d0dbf1576bfc7c81551812d48eeb7f04
Artem Larionenko <lrrrtm.edu@yandex.ru>
2024-09-24 16:34:28 +0300

    moved text to messages.py lexicon from statistics.py

--- tgbot/handlers/statistics.py
+++ tgbot/handlers/statistics.py
@@ -9,7 +9,6 @@ from aiogram.utils.formatting import (
 )
 
 from db.crud import (get_user, get_user_works, get_topic, get_work_questions)
-from tgbot.keyboards.menu import get_menu_kb
 from tgbot.lexicon.messages import lexicon
 from tgbot.lexicon.buttons import lexicon as btns_lexicon
 from utils.user_statistics import get_user_statistics
@@ -21,7 +20,7 @@ router = Router()
 @router.message(F.text == btns_lexicon['main_menu']['stats'])
 async def cmd_stats(message: Message, state: FSMContext):
     msg = await message.answer(
-        text="Собираем твою статистику..."
+        text=lexicon['statistics']['search_for_data']
     )
 
     works_list = get_user_works(message.from_user.id)
@@ -30,17 +29,25 @@ async def cmd_stats(message: Message, state: FSMContext):
     await msg.delete()
 
     if works_list:
-        text_to_send = f"<b>{btns_lexicon['main_menu']['stats']}</b>\n\n"
+        text_to_send = f"<b>{btns_lexicon['main_menu']['stats']}</b>"
         stats_list = get_user_statistics(message.from_user.id)
         for index, stats_el in enumerate(stats_list):
-            text_to_send += (f"{index + 1}. <b>{stats_el['general']['name']} <a href='https://crodconnect.ru/stats?uuid={None}&tid={message.from_user.id}&work={stats_el['general']['work_id']}'>#{stats_el['general']['work_id']}</a></b>"
-                             f"\n    📑 {stats_el['results']['final_mark']}/{stats_el['results']['max_mark']}"
-                             f"\n    ⌛ {stats_el['general']['time']['end'] - stats_el['general']['time']['start']}")
+            text_to_send += lexicon['statistics']['stats_block'].format(
+                index + 1,
+                stats_el['general']['name'],
+                get_user(message.from_user.id).id,
+                message.from_user.id,
+                stats_el['general']['work_id'],
+                stats_el['general']['work_id'],
+                stats_el['results']['final_mark'],
+                stats_el['results']['max_mark'],
+                stats_el['general']['time']['end'] - stats_el['general']['time']['start']
+            )
 
-        text_to_send += "\n\n<b>Нажми на номер задания, который обозначен символом <code>#</code>, чтобы посмотреть подробную статистику по решённому варианту</b>"
+        text_to_send += lexicon['statistics']['hint_to_open_stats']
         await message.answer(text_to_send)
 
     else:
         await message.answer(
-            text=f"У тебя ещё ни одного завершённого задания, чтобы мы могли собрать статистику. \n\nНачни решать, нажав на кнопку <b>«{btns_lexicon['main_menu']['new_work']}»</b>"
+            text=lexicon['statistics']['no_data']
         )
