c1836890579258539a9c91b2e5fe33a020f9bcef
Artem Larionenko <lrrrtm.edu@yandex.ru>
2024-10-01 12:32:57 +0300

    added checking_answers logic for ege questions

--- utils/answer_checker.py
+++ utils/answer_checker.py
@@ -1,9 +1,25 @@
 from db.models import Pool
 
 
+def count_matches(correct_answer, user_answer):
+    matches = 0
+    for i in range(min(len(correct_answer), len(user_answer))):
+        if correct_answer[i] == user_answer[i]:
+            matches += 1
+    return matches
+
+
 def check_answer(qusetion_data: Pool, user_answer: str) -> int:
     if qusetion_data.type == "ege":
-        pass
+        matches = count_matches(qusetion_data.answer, user_answer)
+
+        if matches == 4:
+            return 2
+        elif matches == 3:
+            return 1
+        else:
+            return 0
+
 
     elif qusetion_data.type == "topic":
         if qusetion_data.answer == user_answer:
