2432e6d167140294f97a543c66d0939103a2e44d
Artem Larionenko <lrrrtm.edu@yandex.ru>
2024-10-02 17:54:36 +0300

    update /check

--- tgbot/handlers/check.py
+++ tgbot/handlers/check.py
@@ -8,24 +8,48 @@ from tgbot.handlers.trash import bot
 
 router = Router()
 
-# Список сервисов, состояние которых вы хотите проверять
-services = ['mysql.service', 'chemistry_stats.service', 'chemistry_bot.service']
+services = [
+    {
+        'name': "База данных",
+        'filename': "mysql.service"
+    },
+    {
+        'name': "Telegram-бот",
+        'filename': "mysql.service"
+    },
+    {
+        'name': "Панель статистики",
+        'filename': "mysql.service"
+    }
+]
+
+service_status_translation = {
+    "active": "работает",
+    "inactive": "остановлен",
+    "failed": "завершён с ошибкой",
+    "activating": "активируется",
+    "deactivating": "останавливается",
+    "reloading": "перезагружается",
+    "unknown": "неизвестное состояние"
+}
+
 
 
 async def check_services(message: types.Message):
     result = ""
     for service in services:
         try:
-            status = subprocess.run(['systemctl', 'is-active', service], capture_output=True, text=True)
+            status = subprocess.run(['systemctl', 'is-active', service['filename']], capture_output=True, text=True)
             service_status = status.stdout.strip()
-            result += f"Сервис {service}: {service_status}\n"
+            result += f"<b>{service['name']}:</b> {service_status_translation['service_status']}\n"
         except Exception as e:
-            result += f"Не удалось проверить статус {service}: {e}\n"
+            result += f"{service['filename']}: {e}\n"
 
     await bot.send_message(
         chat_id=message.chat.id,
         text=f"Состояние сервисов:\n{result}",
     )
+
 
 @router.message(Command("check"))
 async def cmd_check(message: types.Message):
