04c5ff6cc5e63f6d9255dd95f370b126c933aad4
Artem Larionenko <lrrrtm.edu@yandex.ru>
2024-10-03 17:20:12 +0300

    ...

--- utils/tags_helper.py
+++ utils/tags_helper.py
@@ -3,4 +3,4 @@ def get_ege_self_check_tags_list():
 
 
 def get_ege_tags_list(each_question_limit: int = 1):
-    return [{'tag': f"ege_{num}", 'limit': each_question_limit} for num in range(27, 35)]
+    return [{'tag': f"ege_{num}", 'limit': each_question_limit} for num in range(1, 35)]
