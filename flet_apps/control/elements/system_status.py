9088a1d3cbfc708deb625519248d6145fcbf8d6a
Artem Larionenko <lrrrtm.edu@yandex.ru>
2024-10-10 17:49:36 +0300

    edit system_status screen

--- flet_apps/control/elements/system_status.py
+++ flet_apps/control/elements/system_status.py
@@ -10,32 +10,32 @@ def get_system_status_col(data: list, on_restart_button_click) -> ft.Column:
         col.controls.append(
             ft.Card(
                 content=ft.Container(
-                    content=ft.Column(
+                    content=ft.Row(
                         controls=[
-                            ft.Row(
-                                controls=[
-                                    ft.Container(
-                                        content=ft.ListTile(
-                                            leading=ft.Icon(el['flet_icon']),
-                                            title=ft.Text(
-                                                value=el['name']
-                                            ),
-                                            subtitle=ft.Text(
-                                                value=el['status']
-                                            ),
-                                            width=280
-                                        ),
-                                        padding=ft.padding.only(left=-15)
+                            ft.Container(
+                                content=ft.ListTile(
+                                    leading=ft.Icon(el['flet_icon']),
+                                    title=ft.Text(
+                                        value=el['name']
                                     ),
-                                    ft.IconButton(
-                                        icon=ft.icons.RESTART_ALT,
-                                        on_click=on_restart_button_click,
-                                        data=el
-                                    )
-                                ],
-                                alignment="center"
+                                    subtitle=ft.Text(
+                                        value=el['status']
+                                    ),
+                                    # width=280
+                                ),
+                                # margin=ft.margin.only(right=-500)
+                                expand=True,
+                            ),
+                            ft.Container(
+                                content=ft.IconButton(
+                                    icon=ft.icons.RESTART_ALT,
+                                    on_click=on_restart_button_click,
+                                    data=el
+                                ),
+                                padding=ft.padding.only(right=10)
                             )
-                        ]
+                        ],
+                        alignment="start"
                     ),
                     padding=10
                 ),
