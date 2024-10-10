5ee7a71004d1c532fe34e2004e53b93aae461426
Artem Larionenko <lrrrtm.edu@yandex.ru>
2024-10-11 01:21:05 +0300

    bug fixes

--- flet_apps/control/columns/users_col.py
+++ flet_apps/control/columns/users_col.py
@@ -12,38 +12,50 @@ def get_users_col(users: List[User], after_user_btn_clicked) -> ft.Column:
     :param after_user_btn_clicked: действие при нажатии кнопки перехода к пользователю
     :return: 
     """
-    col = ft.Column(
-        controls=[
-            ft.Text("Проверка", size=18)
-        ]
-    )
-    return col
+    col = ft.Column()
+
+    # for user in users:
+    #     col.controls.append(
+    #         ft.Card(
+    #             content=ft.Container(
+    #                 content=ft.Row(
+    #                     controls=[
+    #                         # ft.Container(
+    #                         #     content=ft.ListTile(
+    #                         #         leading=ft.Icon(ft.icons.ACCOUNT_CIRCLE),
+    #                         #         title=ft.Text(user.name),
+    #                         #         subtitle=ft.Text(f"id{user.telegram_id}"),
+    #                         #     ),
+    #                         #     expand=True,
+    #                         # ),
+    #                         ft.Container(
+    #                             content=ft.IconButton(
+    #                                 icon=ft.icons.KEYBOARD_ARROW_RIGHT,
+    #                                 on_click=after_user_btn_clicked,
+    #                                 data=user
+    #                             ),
+    #                             padding=ft.padding.only(right=10)
+    #                         )
+    #                     ]
+    #                 ),
+    #                 padding=10
+    #             ),
+    #             width=600
+    #         )
+    #     )
 
     for user in users:
         col.controls.append(
             ft.Card(
-                content=ft.Container(
-                    content=ft.Row(
-                        controls=[
-                            # ft.Container(
-                            #     content=ft.ListTile(
-                            #         leading=ft.Icon(ft.icons.ACCOUNT_CIRCLE),
-                            #         title=ft.Text(user.name),
-                            #         subtitle=ft.Text(f"id{user.telegram_id}"),
-                            #     ),
-                            #     expand=True,
-                            # ),
-                            ft.Container(
-                                content=ft.IconButton(
-                                    icon=ft.icons.KEYBOARD_ARROW_RIGHT,
-                                    on_click=after_user_btn_clicked,
-                                    data=user
-                                ),
-                                padding=ft.padding.only(right=10)
-                            )
-                        ]
-                    ),
-                    padding=10
+                content=ft.Column(
+                    controls=[
+                        ft.Text(user.name),
+                        ft.ElevatedButton(
+                            text="Открыть",
+                            data=user,
+                            on_click=after_user_btn_clicked,
+                        )
+                    ]
                 ),
                 width=600
             )
