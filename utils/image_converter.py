b2400ac9e9a7473389e78101e92c9f3c26557ce0
Artem Larionenko <lrrrtm.edu@yandex.ru>
2024-11-09 21:36:19 +0300

    ...

--- utils/image_converter.py
+++ utils/image_converter.py
@@ -2,11 +2,15 @@ import os
 import base64
 
 
-def image_to_base64(image_type: str, question_id: int):
+def image_to_base64(image_type: str, question_id):
+    image_path = 'temp'
+
     if image_type == "question":
         image_path = os.path.join(os.getenv('ROOT_FOLDER'), f"data/questions_images/{question_id}.png")
     elif image_type == "answer":
         image_path = os.path.join(os.getenv('ROOT_FOLDER'), f"data/answers_images/{question_id}.png")
+    elif image_type == "temp":
+        image_path = os.path.join(os.getenv('ROOT_FOLDER'), f"data/temp/{question_id}.png")
 
     with open(image_path, "rb") as image_file:
         base64_image = base64.b64encode(image_file.read()).decode('utf-8')
