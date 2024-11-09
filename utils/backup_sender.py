198247efc36baac422115fd5215673fb62e97ce4
Artem Larionenko <lrrrtm.edu@yandex.ru>
2024-11-09 19:29:56 +0300

    ...

--- utils/backup_sender.py
+++ utils/backup_sender.py
@@ -10,13 +10,12 @@ bot = telebot.TeleBot(token=getenv('BOT_API_KEY'), parse_mode="html")
 
 
 def check_file_created_today(directory):
-    today = time.localtime()  # Текущая дата
-    today_date = time.strftime("%Y-%m-%d", today)  # Форматируем как YYYY-MM-DD
+    today = time.localtime()
+    today_date = time.strftime("%Y-%m-%d", today)
 
     for filename in listdir(directory):
         file_path = path.join(directory, filename)
 
-        # Получаем время последнего изменения файла
         file_creation_time = time.localtime(path.getctime(file_path))
         file_date = time.strftime("%Y-%m-%d", file_creation_time)
 
@@ -31,12 +30,21 @@ directory_path = "/root/backups"
 data = check_file_created_today(directory_path)
 if data is not None:
     bot.send_document(
-        chat_id="409801981",
+        chat_id=getenv('ADMIN_ID'),
+        document=open(path.join(directory_path, data), "rb"),
+        caption="<b>ℹ️ Сервисные сообщения</b>\n\nРезервная копия базы данных"
+    )
+    bot.send_document(
+        chat_id=getenv('DEVELOPER_ID'),
         document=open(path.join(directory_path, data), "rb"),
         caption="<b>ℹ️ Сервисные сообщения</b>\n\nРезервная копия базы данных"
     )
 else:
     bot.send_message(
-        chat_id="409801981",
+        chat_id=getenv('ADMIN_ID'),
+        text="<b>ℹ️ Сервисные сообщения</b>\n\nРезервная копия базы данных не обнаружена, обратитесь к администратору"
+    )
+    bot.send_message(
+        chat_id=getenv('DEVELOPER_ID'),
         text="<b>ℹ️ Сервисные сообщения</b>\n\nРезервная копия базы данных не обнаружена, обратитесь к администратору"
     )
