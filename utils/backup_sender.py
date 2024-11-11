f6901451badc85d4c46a3675b54d739a3fe86448
Artem Larionenko <lrrrtm.edu@yandex.ru>
2024-11-11 16:49:39 +0300

    ...

--- utils/backup_sender.py
+++ utils/backup_sender.py
@@ -1,21 +1,23 @@
+import os.path
 from os import getenv, listdir, path
 from dotenv import load_dotenv
-
 import telebot
 import time
+import sys
 
+# Load environment variables
 load_dotenv()
 
+# Initialize bot with API token
 bot = telebot.TeleBot(token=getenv('BOT_API_KEY'), parse_mode="html")
-
 
 def check_file_created_today(directory):
     today = time.localtime()
     today_date = time.strftime("%Y-%m-%d", today)
 
+    # Check each file in the directory
     for filename in listdir(directory):
         file_path = path.join(directory, filename)
-
         file_creation_time = time.localtime(path.getctime(file_path))
         file_date = time.strftime("%Y-%m-%d", file_creation_time)
 
@@ -24,22 +26,30 @@ def check_file_created_today(directory):
 
     return None
 
+# Get directory path from command-line arguments
+if len(sys.argv) < 2:
+    print("Usage: python3 backup_sender.py <dir>")
+    sys.exit(1)
 
-directory_path = "/root/backups"
+directory_path = sys.argv[1]
 
-data = check_file_created_today(directory_path)
-if data is not None:
-    bot.send_document(
-        chat_id=getenv('ADMIN_ID'),
-        document=open(path.join(directory_path, data), "rb"),
-        caption="<b>ℹ️ Сервисные сообщения</b>\n\nРезервная копия базы данных"
-    )
-    bot.send_document(
-        chat_id=getenv('DEVELOPER_ID'),
-        document=open(path.join(directory_path, data), "rb"),
-        caption="<b>ℹ️ Сервисные сообщения</b>\n\nРезервная копия базы данных"
-    )
+# Check if the file was created today
+# data = check_file_created_today(directory_path)
+if os.path.exists(directory_path):
+    # Send the document to ADMIN and DEVELOPER
+    with open(directory_path, "rb") as document:
+        bot.send_document(
+            chat_id=getenv('ADMIN_ID'),
+            document=document,
+            caption="<b>ℹ️ Сервисные сообщения</b>\n\nРезервная копия базы данных"
+        )
+        bot.send_document(
+            chat_id=getenv('DEVELOPER_ID'),
+            document=document,
+            caption="<b>ℹ️ Сервисные сообщения</b>\n\nРезервная копия базы данных"
+        )
 else:
+    # Send a message if no backup file is found
     bot.send_message(
         chat_id=getenv('ADMIN_ID'),
         text="<b>ℹ️ Сервисные сообщения</b>\n\nРезервная копия базы данных не обнаружена, обратитесь к администратору"
